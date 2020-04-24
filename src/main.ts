4ec693e7904a3bb7ae2aaeafa21977fca0d89760
mro <254225961@qq.com>
2020-04-24 18:22:04 +0800

    feat: format4 action yaml

--- src/main.ts
+++ src/main.ts
@@ -5,16 +5,25 @@ import { githubPublish } from './github-publish';
 
 async function run(): Promise<void> {
   const workspaces = core.getInput('workspaces').split(',');
-
-  // npm is required
-  await exec.exec('npm', ['--version']);
-
   try {
-    const manifests = await Promise.all(workspaces.map(npmPublish));
-    if (!process.env.GITHUB_TOKEN) return;
-    githubPublish(manifests);
+    // npm is required
+    await exec.exec('npm', ['--version']);
   } catch (error) {
-    core.setFailed(error);
+    core.setFailed('npm is not install');
+  }
+
+  let manifests;
+  try {
+    manifests = await Promise.all(workspaces.map(npmPublish));
+  } catch (error) {
+    core.setFailed("npm publish fail" + error);
+  }
+
+  if (!process.env.GITHUB_TOKEN) return;
+  try {
+    await githubPublish(manifests ?? []);
+  } catch (error) {
+    core.setFailed("github publish fail" + error);    
   }
 }
 
